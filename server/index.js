c0255d6e51efa7e533481fe6e119859614957b65
antmedina <ant.n.medina@gmail.com>
2017-10-23 10:39:23 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+var path = require('path');
+var express = require('express');
+var bodyParser =require('body-parser');
+
+var app = express();
+
+var clientPath = path.join(__dirname, "../Client");
+
+app.use(express.static(clientPath));
+app.listen(process.env.PORT || 3000);
+
