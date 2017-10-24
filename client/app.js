7d68cd7320363907a5b103354e7741a0b4a157c4
“ddmerci” <31666738+ddmerci@users.noreply.github.com>
2017-10-24 14:30:44 -0400

    stripe-form added to checkout & comment changes

--- client/app.js
+++ client/app.js
@@ -1,23 +1,23 @@
-angular.module('store', ['ngRoute', 'ngResource', 'store.controllers', 'store.factories', 'store.services'])
+angular.module('store', ['ngRoute', 'ngResource', 'store.controllers', 'store.factories', 'store.services', 'angularPayments'])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
     .when('/', { 
         templateUrl: 'views/welcome.html'
     })
-    .when('/products/merch', { //needs to match api route
+    .when('/products/merch', {
         templateUrl: 'views/merch.html',
         controller: 'ProductsController' 
     })
-    .when('/products/apparel', { //needs to match api route
+    .when('/products/apparel', { 
         templateUrl: 'views/apparel.html',
         controller: 'ProductsController' 
     })
-    .when('/purchases', { //needs to match api route
+    .when('/purchases', {
         templateUrl: 'views/cart.html',
         controller: "PurchasesController" 
     })
-    .when('/products/:id', { //needs to match api route
+    .when('/products/:id', { 
         templateUrl: 'views/detail.html',
         controller: 'DetailController' 
     })
