1767525c794575a922f0b24a817200d80d2b69f5
egarcia07x <31666808+egarcia07x@users.noreply.github.com>
2017-10-24 20:54:53 -0400

    ...

---
+++ client/controllers.js
@@ -0,0 +1,22 @@
+
+angular.module('store.controllers', [])
+
+    .controller('welcomeController', ['$scope', '$location', '$routeParams', 'SEOService', function ($scope, $location, $routeparams,  SEOService) {
+
+
+    }])
+
+    .controller('apparelController', ['$scope', 'Products', 'Purchases', '$location', '$routeParams',  'SEOService', function ($scope, Products, Purchases, $location, $routeparams,  SEOService) {
+        $scope.product = Products.queryByCategory({ categoryid: 1 });
+    }])
+
+    .controller('merchController', ['$scope', 'Products', 'Purchases', '$location', '$routeParams',  'SEOService', function ($scope, Products, Purchases, $location, $routeparams, SEOService) {
+        $scope.product = Products.queryByCategory({ categoryid: 2 });
+    }])
+
+    .controller('checkoutController', ['$scope', 'Products', 'Purchases', '$location', '$routeParams',  'SEOService', function ($scope, Products, Purchases, $location, $routeparams,  SEOService) {
+
+    }])
+
+
+    .controller('SingleProductController', ['$scope', 'Products', 'Purchases', '$location', '$routeParams', 'SEOService', function ($scope, Products, Purchases, $location, $routeparams,  SEOService) {
