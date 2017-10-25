571e3d3d7181a916bcd44cbb7210740834bb9ce7
antmedina <ant.n.medina@gmail.com>
2017-10-25 09:19:41 -0400

    prep for day 3 merge

--- client/services.js
+++
@@ -1,9 +0,0 @@
-angular.module('store.services', [])
-    .service('SEOService', ['$rootScope', function ($rootScope) {
-        this.setSEO = function (data) {
-            $rootScope.seo = {};
-            for (var p in data) {
-                $rootScope.seo[p] = data[p];
-            }
-        }
-    }]);
--- server/procedures/purchases.proc.js
+++
@@ -1,8 +0,0 @@
-var db = require('../config/db');
-
-
-
-exports.create = function (productid, price, stripetransactionid) {
-    return db.row('', [productid, price, stripetransactionid]);
-}
-
