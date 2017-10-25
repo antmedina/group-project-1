6685bb1d1e54f7c7a205ce4f4602b07ab16be05f
sam quave <samquave@sams-air.chattanoogatn.gov>
2017-10-25 12:19:29 -0400

    post api route for purchases

--- server/procedures/purchases.proc.js
+++ server/procedures/purchases.proc.js
@@ -2,7 +2,7 @@ var db = require('../config/db');
 
 
 
-exports.create = function (productid, price, stripetransactionid) {
-    return db.row('', [productid, price, stripetransactionid]);
+exports.create = function (price, stripetransactionid) {
+    return db.row('addNewPurchase', [price, stripetransactionid]);
 }
 
