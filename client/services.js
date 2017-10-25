01f9fa2ab05af6bcde023ba51b589132d511c423
antmedina <ant.n.medina@gmail.com>
2017-10-24 22:02:03 -0400

    add final night test via upload
    FAAAACK!!!

--- client/services.js
+++ client/services.js
@@ -1,1 +1,9 @@
-angular.module('store.services', []);
+angular.module('Store.services', [])
+    .service('SEOService', ['$rootScope', function ($rootScope) {
+        this.setSEO = function (data) {
+            $rootScope.seo = {};
+            for (var p in data) {
+                $rootScope.seo[p] = data[p];
+            }
+        }
+    }]);
