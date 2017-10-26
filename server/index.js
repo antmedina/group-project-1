66d52d5d23b3b2c9531e40bae0809638acdf6d42
antmedina <ant.n.medina@gmail.com>
2017-10-26 15:28:58 -0400

    stripe testing t3

--- server/index.js
+++ server/index.js
@@ -18,5 +18,34 @@ app.use(bodyParser.json());
 app.use('/api', api);
 app.get('*', routing.stateRouting);
 
+// stripe attempt//
+var stripe = require('stripe')('sk_testSECRET_KEY');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/success', function(req, res) {
+  res.send('order completed');
+});
+
+app.post('/charge', function(req, res) {
+    var stripeToken = req.body.stripeToken;
+    var amount = req.body.amount;
+
+    stripe.charges.create({
+        card: stripeToken,
+        currency: 'usd',
+        amount: amount
+    },
+    function(err, charge) {
+        if (err) {
+            res.send(500, err);
+        } else {
+            res.send('total charged is:' + charge);
+        }
+    });
+});
+
+app.use(express.static(__dirname));
+//stripe attempt//
 
 app.listen(process.env.PORT || 3000);
